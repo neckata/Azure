ae0676f7fc7dec3a95d977ace84097fdc30fe3d4
Nikola Netsov <pikedbs@abv.bg>
2021-03-06 14:02:24 +0200

    added table

---
+++ Azure/StorageQueue/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Threading.Tasks;
+
+namespace StorageQueue
+{
+    class Program
+    {
+        private static string queue_connection_string = "";
+        private static string queue_name = "appqueue";
+        static async void Main(string[] args)
+        {
+            await Send();
+            await Recieve();
+        }
+
+        static async Task Send()
+        {
+            CloudQueue _queue = GetCloudQueue();
+
+            for (int i = 1; i < 10; i++)
+            {
+                Order obj = new Order();
+                CloudQueueMessage _message = new CloudQueueMessage(obj.ToString());
+                await _queue.AddMessageAsync(_message);
+            }
+
+            Console.WriteLine("All messages sent");
+            Console.ReadLine();
+        }
+
+        static async Task Recieve()
+        {
+            CloudQueue _queue = GetCloudQueue();
+
+            await _queue.FetchAttributesAsync();
+            int? _count = _queue.ApproximateMessageCount;
+
+            for (int i = 0; i < _count; i++)
+            {
+                CloudQueueMessage _message = await _queue.GetMessageAsync();
+                Console.WriteLine(_message.AsString);
+                await _queue.DeleteMessageAsync(_message);
+            }
+            Console.ReadLine();
+        }
+
+        static CloudQueue GetCloudQueue()
+        {
+            CloudStorageAccount queue_acc = CloudStorageAccount.Parse(queue_connection_string);
+
+            CloudQueueClient queue_client = queue_acc.CreateCloudQueueClient();
+
+            CloudQueue _queue = queue_client.GetQueueReference(queue_name);
+
+            return _queue;
+        }
+    }
+}
---
+++ Azure/TableStorage/Customer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableStorage
+{
+    class Customer : TableEntity
+    {
+        public string city { get; set; }
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(string p_id, string p_name, string p_city)
+        {
+            PartitionKey = p_id;
+            RowKey = p_name;
+            city = p_city;
+        }
+    }
+}
